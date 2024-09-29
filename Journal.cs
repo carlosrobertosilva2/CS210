d14c714f402bda176d619bbb18bd5b646f703494
Carlos Roberto Silva <csilva@graniteschools.org>
2024-09-29 00:06:59 -0600

    Journal.cs
    Journal.cs manage the list of entries.. Journal.cs (for the Journal class)

---
+++ Journal.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+public class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+        _entries.Add(entry);
+    }
+
+    public void DisplayEntries()
+    {
+        foreach (var entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+}
